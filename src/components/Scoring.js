c79fa4082b8984200cfca58f19e48a51edea05fe
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-07 17:11:26 +1100

    partial code for _AddScore

--- src/components/Scoring.js
+++ src/components/Scoring.js
@@ -12,22 +12,62 @@ const labelStyle = {
     }
 }
 
+const labels = {
+    opponentOne: "Opponent 1",
+    opponentTwo: "Opponent 2",
+}
+
 class Scoring extends Component {
     constructor() {
         super()
+        
+        this._AddScore = this._AddScore.bind(this)
+
+        this.state = {
+            opponentOne: 0,
+            opponentTwo: 0,
+        }
     }
 
-    state = {
-        opponentOne: 0,
-        opponentTwo: 0,
+
+
+    _AddScore(e){
+        switch (e.target.innerHTML) {
+            case labels.opponentOne:
+                console.log('op 1')
+                break
+            case labels.opponentTwo:
+                console.log('op 2')
+                break
+            default:
+                console.log('error')
+        }
+            
+
+        // console.log(e.target.innerHTML)
+        // this.setState({
+        //     opponentOne: this.state.opponentOne + 1,
+        // })
+
     }
 
     render(){
         return(
             <App>
-                <FlatButton label="Opponent 1" style={labelStyle.scoring}/>
-                <FlatButton label="Opponent 2" style={labelStyle.scoring}/>
-                <br />
+                <FlatButton 
+                    label={labels.opponentOne}
+                    style={labelStyle.scoring} 
+                    name="Op-one"
+                    onClick={(e) => this._AddScore(e)}
+                />
+                <FlatButton 
+                    label={labels.opponentTwo}
+                    style={labelStyle.scoring} 
+                    name="Op-two"
+                    onClick={(e) => this._AddScore(e)}
+                />
+
+                <br/>
                 {this.state.opponentOne + ' : ' + this.state.opponentTwo}
             </App> 
         )
